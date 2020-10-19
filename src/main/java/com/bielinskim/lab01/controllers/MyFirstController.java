20e67806c73fdaca91e126be92c9288279ad5bc3
Mateusz Bieliñski <mateusz100022@gmail.com>
2020-10-19 22:48:12 +0200

    Initial commit

---
+++ src/main/java/com/bielinskim/lab01/controllers/MyFirstController.java
@@ -0,0 +1,54 @@
+package com.bielinskim.lab01.controllers;
+
+import com.bielinskim.lab01.models.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@Controller
+public class MyFirstController {
+
+    @GetMapping("/page1.html")
+    public void firstControllerMethod(HttpServletResponse response) throws IOException {
+        response.getWriter().write(String.valueOf(java.time.LocalDate.now()));
+    }
+
+    @GetMapping("/page2")
+    public String secondControllerMethod(Model model){
+        model.addAttribute("first_name", "Mateusz");
+        model.addAttribute("last_name", "Bieliński");
+        return "page2";
+    }
+
+    @GetMapping("/page3")
+    public ModelAndView thirdControllerMethod(){
+        var mav = new ModelAndView( "page3");
+        mav.addObject("os_arch", System.getProperty("os.arch") );
+        mav.addObject("os_name", System.getProperty("os.name") );
+        mav.addObject("java_vendor", System.getProperty("java.vendor") );
+        mav.addObject("java_version", System.getProperty("java.version") );
+
+        return mav;
+
+    }
+    @GetMapping("/book")
+    public ModelAndView getBookController(){
+        var mav = new ModelAndView( "book");
+       var book = new Book();
+       book.setTitle("W pustyni i w puszczy");
+       book.setAuthor("Henryk Sienkiewicz");
+       book.setReleaseDate(LocalDate.of(2005, 12, 23));
+       book.setPrice(39.00f);
+       book.setBestseller(true);
+       mav.addObject(book);
+
+        return mav;
+
+    }
+
+}
---
+++ src/main/java/com/bielinskim/lab01/models/Book.java
@@ -0,0 +1,60 @@
+package com.bielinskim.lab01.models;
+
+import java.time.LocalDate;
+
+public class Book {
+    long id;
+    String author;
+    String title;
+    LocalDate releaseDate;
+    Float price;
+    Boolean bestseller;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Boolean getBestseller() {
+        return bestseller;
+    }
+
+    public void setBestseller(Boolean bestseller) {
+        this.bestseller = bestseller;
+    }
+}
